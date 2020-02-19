1be52aca5793b7f5040a54989de3ba216eee091e
Nick Magnusson <Nikita.magnusson@live.se>
2020-02-19 12:11:44 +0100

    Cards

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -21,6 +21,7 @@ import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+
 const useTabs = makeStyles({
   root: {
     flexGrow: 1
@@ -70,11 +71,17 @@ export default function CenteredTabs() {
 
 const useStyles = makeStyles(theme => ({
   root: {
-    maxWidth: 345
+    maxWidth: 250
   },
   media: {
     height: 0,
     paddingTop: '56.25%' // 16:9
+  },
+  typography: {
+    color: 'white'
+  },
+  title: {
+    color: 'red',
   },
   expand: {
     transform: 'rotate(0deg)',
@@ -100,11 +107,11 @@ export function RecipeReviewCard() {
   };
 
   return (
-    <Card className={classes.root}>
+    <Card className={classes.root} style={{ backgroundColor: '#271F1F' }}>
       <CardHeader
         avatar={
           <Avatar aria-label="recipe" className={classes.avatar}>
-            R
+            G
           </Avatar>
         }
         action={
@@ -116,16 +123,15 @@ export function RecipeReviewCard() {
         subheader="CSGO TEAM"
       />
       <CardMedia
+        component="img"
         className={classes.media.MuiCardMedia}
-        style={{ height: 0, paddingTop: '56.25%' }}
-        img
-        src="https://upload.wikimedia.org/wikipedia/en/3/36/Godsent_logo.png"
+        height="200"
+        image="https://upload.wikimedia.org/wikipedia/en/3/36/Godsent_logo.png"
         title="GODSENT"
       />
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        <Typography variant="body2" color='grey[500]' lineHeight={1.8} fontSize={12} component="p">
+        Godsent is a Swedish esports organization formed by Markus "pronax" Wallsten and entrepreneur Tomas Oropesa Martin. In September 2019, The Final Tribe merged with Godsent.[1]
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -148,31 +154,22 @@ export function RecipeReviewCard() {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>Latest Games:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
+            Matches
           </Typography>
           <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            Matches
           </Typography>
           <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
+            Matches
           </Typography>
           <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+            Matches
           </Typography>
         </CardContent>
       </Collapse>
     </Card>
   );
 }
+ 
