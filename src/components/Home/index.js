276af55be6d7158b68d04792c2f311db213223c9
fredrik1998 <fredrik.andren@student.kyh.se>
2020-02-26 14:16:15 +0100

    Fix styling to Dashboard page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,8 @@
+// import React from 'react';
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import { Bar, Doughnut, Line } from 'react-chartjs-2';
 import { findPrizePool, tournaments } from '../Data/tournaments';
 import { makeBarGraph, makeDonutGraph, makeLineGraph } from '../Chartjs/chartHelpers';
@@ -8,61 +12,107 @@ import styled from 'styled-components';
 import DoughnutDemo from '../Chartjs/Doughnut';
 import KDA from '../Chartjs/kdaBar';
 import './styles.css';
-import Grid from '@material-ui/core/Grid';
+// import Grid from '@material-ui/core/Grid';
 const prizeData = findPrizePool();
 const data = makeLineGraph(prizeData);
 
 
 
-const StyledGrid = styled(Grid)`
-display: flex;
-justify-content: space-around;
-flex-direction: row-reverse;
-`
+// const StyledGrid = styled(Grid)`
+// display: flex;
+// justify-content: space-around;
+// flex-direction: row-reverse;`
 
 
 
-export default class Home extends React.Component {
+// export default class Home extends React.Component {
 
-  render() {
+//   render() {
     const options = {
 
-      maintainAspectRatio: false,
-      responsive: false,
+      maintainAspectRatio: true,
+      responsive: true,
       legend: {
         display: true
       },
-      type: "Doughnut", 
+     
     }
 
-    return (
-      <div>
-        <Line
+
+
+//     return (
+//       <div>
+//         <Line
+//          data={makeLineGraph(prizeData)}
+//          width={500}
+//          height={500}
+//          options={options}
+
+//         />
+//         <StyledGrid>
+        // <Doughnut
+        //    data={makeDonutGraph(weaponLabels, weaponData)}
+        //    width={500}
+        //    height={500}
+        //    options={options}
+        // />
+        // <Bar
+        //   data={makeBarGraph().data}
+        //   width={500}
+        //   height={500}
+        //   options={options}
+        // />
+//         </StyledGrid>
+
+
+
+//       </div>
+
+//     );
+//   }
+// }
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+ 
+}));
+export default function FullWidthGrid() {
+  const classes = useStyles();
+  
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} zeroMinWidth >
+          <Line
          data={makeLineGraph(prizeData)}
-         width={1800}
-         height={400}
+         width={500}
+         height={100}
          options={options}
 
         />
-        <StyledGrid>
-        <Doughnut
+        </Grid>
+        <Grid item xs={12} sm={6}>
+        <Doughnut 
            data={makeDonutGraph(weaponLabels, weaponData)}
-           width={400}
-           height={500}
+           width={500}
+           height={225}
            options={options}
         />
+        </Grid>
+        <Grid item xs={12} sm={6}>
         <Bar
           data={makeBarGraph().data}
-          width={600}
-          height={275}
+          width={500}
+          height={225}
           options={options}
         />
-        </StyledGrid>
-
-
-
-      </div>
-
-    );
-  }
+        </Grid>
+        </Grid>
+      
+      
+    </div>
+  );
 }
