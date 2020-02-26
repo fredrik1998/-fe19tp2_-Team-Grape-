e5bedcbb354c613e1e5ad4505db9a6d70585eb22
fredrik1998 <fredrik.andren@student.kyh.se>
2020-02-26 12:04:51 +0100

    Added styling to Charts

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,78 +1,28 @@
 import React from 'react';
 import { Bar, Doughnut, Line } from 'react-chartjs-2';
 import { findPrizePool, tournaments } from '../Data/tournaments';
-import { makeBarGraph } from '../Chartjs/chartHelpers';
-
+import { makeBarGraph, makeDonutGraph, makeLineGraph } from '../Chartjs/chartHelpers';
+import { weaponLabels, weaponData } from '../Data/gunstats';
 import LineDemo from '../Chartjs/linegraph';
 import styled from 'styled-components';
 import DoughnutDemo from '../Chartjs/Doughnut';
 import KDA from '../Chartjs/kdaBar';
 import './styles.css';
 import Grid from '@material-ui/core/Grid';
+const prizeData = findPrizePool();
+const data = makeLineGraph(prizeData);
 
 
 
 const StyledGrid = styled(Grid)`
 display: flex;
-flex-direction: row-reverse;`
+justify-content: space-around;
+flex-direction: row-reverse;
+`
 
 
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July"
-        ],
-        datasets: [
-          {
-            label: "My First dataset",
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            borderWidth: 1,
-            //stack: 1,
-            hoverBackgroundColor: "rgba(255,99,132,0.4)",
-            hoverBorderColor: "rgba(255,99,132,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-          },
-
-          {
-            label: "My second dataset",
-            backgroundColor: "rgba(155,231,91,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            borderWidth: 1,
-            //stack: 1,
-            hoverBackgroundColor: "rgba(255,99,132,0.4)",
-            hoverBorderColor: "rgba(255,99,132,1)",
-            data: [45, 79, 50, 41, 16, 85, 20]
-          },
-          {
-            label: "My First dataset",
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            borderWidth: 1,
-            //stack: 1,
-            hoverBackgroundColor: "rgba(255,99,132,0.4)",
-            hoverBorderColor: "rgba(255,99,132,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-          },
-        ]
-      }
-    };
-  }
-
-  componentDidMount() {
-    console.log(makeBarGraph());
-    console.log(this.state.data)
-  }
 
   render() {
     const options = {
@@ -80,28 +30,34 @@ export default class Home extends React.Component {
       maintainAspectRatio: false,
       responsive: false,
       legend: {
-        display: false
+        display: true
       },
-      type: "Doughnut",
+      type: "Doughnut", 
     }
 
     return (
       <div>
-        <LineDemo
+        <Line
+         data={makeLineGraph(prizeData)}
+         width={1800}
+         height={400}
+         options={options}
 
         />
-        <DoughnutDemo
-          options={{
-            responsive: true,
-            maintainAspectRatio: true,
-          }}
+        <StyledGrid>
+        <Doughnut
+           data={makeDonutGraph(weaponLabels, weaponData)}
+           width={400}
+           height={500}
+           options={options}
         />
         <Bar
           data={makeBarGraph().data}
-          width={1000}
-          height={1000}
+          width={600}
+          height={275}
           options={options}
         />
+        </StyledGrid>
 
 
 
